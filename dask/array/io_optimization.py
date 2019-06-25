27a5aa7edee9fa5da6bc69bbcca8176d334c3cd7
GTimothee <ava_punksmash@protonmail.com>
2019-06-25 11:14:53 -0400

    new cleaner version

--- dask/array/io_optimization.py
+++ dask/array/io_optimization.py
@@ -3,13 +3,16 @@ import math
 import numpy as np
 import sys
 sys.path.insert(0,'/home/user/Documents/workspace/projects/samActivities/tests/optimize_io')
-
+import time 
 import optimize_io
 from optimize_io.main import *
 
 def optimize_func(dsk, keys):
+    t = time.time()
     dask_graph = dsk.dicts
     dask_graph = main(dask_graph)
+    t = time.time() - t
+    print("time to create graph:", t)
     return dsk
 
 
